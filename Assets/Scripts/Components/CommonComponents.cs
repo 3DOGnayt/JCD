763ef742c14d5eceaccf4b55ec1017bfec34f7a7
3DOGnayt <76401844+3DOGnayt@users.noreply.github.com>
2025-09-08 23:17:31 +0300

    JCD-Start-Proccess add CommonComponents and GameCarComponents

--- Assets/Scripts/Components/CommonComponents.cs
+++ Assets/Scripts/Components/CommonComponents.cs
@@ -6,7 +6,7 @@ namespace Components
     //CommonComponents
     public struct TransformComponent : IComponent { public Transform Value; }
     public struct PositionComponent : IComponent { public Vector3 Value; }
-    public struct RotationComponent : IComponent { public Vector3 Value; }
+    public struct RotationComponent : IComponent { public Quaternion Value; }
     public struct ScaleComponent : IComponent { public float Value; }
     
     //Tags
--- Assets/Scripts/Systems/PlayerSpawnSystem.cs
+++ Assets/Scripts/Systems/PlayerSpawnSystem.cs
@@ -31,82 +31,111 @@ namespace Systems
             var instance = _container.InstantiatePrefab(player, Vector3.zero, Quaternion.identity, _playerGroup);
 
             var entity = World.CreateEntity();
-            AddGameComponents(entity, instance.transform);
-            AddInternalComponents(entity);
-            entity.SetComponent(new PlayerTagComponent());
+            AddGameComponents(entity);
+            AddInternalComponents(entity, instance.transform);
         }
 
-        private void AddGameComponents(Entity entity, Transform instanceTransform)
+        private void AddGameComponents(Entity entity)
         {
+            entity.SetComponent(new PlayerTagComponent());
+            
             entity.SetComponent(new SpeedComponent { Value = 100});
+            entity.SetComponent(new BackSpeedComponent { Value = 40});
+            entity.SetComponent(new AccelerationMultiplierComponent { Value = 4});
+            entity.SetComponent(new DecelerationMultiplierComponent { Value = 2});
+            entity.SetComponent(new SteeringAngleComponent { Value = 40});
+            entity.SetComponent(new SteeringSpeedComponent { Value = 1});
+            entity.SetComponent(new BrakeForceComponent { Value = 1});
+            entity.SetComponent(new DriftMultiplierComponent { Value = 1});
         }
 
-        private void AddInternalComponents(Entity entity)
+        private void AddInternalComponents(Entity entity, Transform car)
+        {
+            AddCommonComponents(entity, car);
+            AddMainCarComponents(entity);
+            AddFrontWheelComponents(entity);
+            AddBackWheelComponents(entity);
+        }
+
+        private void AddCommonComponents(Entity entity, Transform car)
+        {
+            entity.SetComponent(new PlayerTagComponent());
+
+            entity.SetComponent(new TransformComponent { Value = car });
+            entity.SetComponent(new PositionComponent { Value = car.position });
+            entity.SetComponent(new RotationComponent { Value = car.rotation });
+            entity.SetComponent(new ScaleComponent { Value = 1 });
+        }
+
+        private void AddMainCarComponents(Entity entity)
         {
             var carParameters = _carPreset.CarParameters;
-            
-            // Main components
-            entity.SetComponent(new CarMassComponent{ Value = carParameters.Mass});
-            entity.SetComponent(new AutomaticCenterOfMassComponent{ Value = carParameters.AutomaticCenterOfMass});
-            entity.SetComponent(new CenterOfMassComponent{ Value = carParameters.CenterOfMass});
-            
+
+            entity.SetComponent(new CarMassComponent { Value = carParameters.Mass });
+            entity.SetComponent(new AutomaticCenterOfMassComponent { Value = carParameters.AutomaticCenterOfMass });
+            entity.SetComponent(new CenterOfMassComponent { Value = carParameters.CenterOfMass });
+        }
+
+        private void AddFrontWheelComponents(Entity entity)
+        {
             var frontMainWheelParameters = _carPreset.FrontWheelParameters.MainWheelParameters;
             var frontSuspensionSpring = _carPreset.FrontWheelParameters.SuspensionSpring;
             var frontForwardFriction = _carPreset.FrontWheelParameters.ForwardFriction;
             var frontSidewaysFriction = _carPreset.FrontWheelParameters.SidewaysFriction;
-            
-            // Front components
-            entity.SetComponent(new FrontWheelMassComponent{ Value = frontMainWheelParameters.Mass});
-            entity.SetComponent(new FrontWheelRadiusComponent{ Value = frontMainWheelParameters.Radius});
-            entity.SetComponent(new FrontDampingRateComponent{ Value = frontMainWheelParameters.DampingRate});
-            entity.SetComponent(new FrontSuspensionDistanceComponent{ Value = frontMainWheelParameters.SuspensionDistance});
-            entity.SetComponent(new FrontForceAppPointDistanceComponent{ Value = frontMainWheelParameters.ForceAppPointDistance});
-            entity.SetComponent(new FrontWheelCenterComponent{ Value = frontMainWheelParameters.Center});
-            
-            entity.SetComponent(new FrontSpringComponent{ Value = frontSuspensionSpring.Spring});
-            entity.SetComponent(new FrontDamperComponent{ Value = frontSuspensionSpring.Damper});
-            entity.SetComponent(new FrontTargetPositionComponent{ Value = frontSuspensionSpring.TargetPosition});
-            
-            entity.SetComponent(new FrontExtremumSlipForwardComponent{ Value = frontForwardFriction.ExtremumSlip});
-            entity.SetComponent(new FrontExtremumValueForwardComponent{ Value = frontForwardFriction.ExtremumValue});
-            entity.SetComponent(new FrontAsymptoteSlipForwardComponent{ Value = frontForwardFriction.AsymptoteSlip});
-            entity.SetComponent(new FrontAsymptoteValueForwardComponent{ Value = frontForwardFriction.AsymptoteValue});
-            entity.SetComponent(new FrontStiffnessForwardComponent{ Value = frontForwardFriction.Stiffness});
-            
-            entity.SetComponent(new FrontExtremumSlipSidewaysComponent{ Value = frontSidewaysFriction.ExtremumSlip});
-            entity.SetComponent(new FrontExtremumValueSidewaysComponent{ Value = frontSidewaysFriction.ExtremumValue});
-            entity.SetComponent(new FrontAsymptoteSlipSidewaysComponent{ Value = frontSidewaysFriction.AsymptoteSlip});
-            entity.SetComponent(new FrontAsymptoteValueSidewaysComponent{ Value = frontSidewaysFriction.AsymptoteValue});
-            entity.SetComponent(new FrontStiffnessSidewaysComponent{ Value = frontSidewaysFriction.Stiffness});
-            
+
+            entity.SetComponent(new FrontWheelMassComponent { Value = frontMainWheelParameters.Mass });
+            entity.SetComponent(new FrontWheelRadiusComponent { Value = frontMainWheelParameters.Radius });
+            entity.SetComponent(new FrontDampingRateComponent { Value = frontMainWheelParameters.DampingRate });
+            entity.SetComponent(new FrontSuspensionDistanceComponent { Value = frontMainWheelParameters.SuspensionDistance });
+            entity.SetComponent(new FrontForceAppPointDistanceComponent { Value = frontMainWheelParameters.ForceAppPointDistance });
+            entity.SetComponent(new FrontWheelCenterComponent { Value = frontMainWheelParameters.Center });
+
+            entity.SetComponent(new FrontSpringComponent { Value = frontSuspensionSpring.Spring });
+            entity.SetComponent(new FrontDamperComponent { Value = frontSuspensionSpring.Damper });
+            entity.SetComponent(new FrontTargetPositionComponent { Value = frontSuspensionSpring.TargetPosition });
+
+            entity.SetComponent(new FrontExtremumSlipForwardComponent { Value = frontForwardFriction.ExtremumSlip });
+            entity.SetComponent(new FrontExtremumValueForwardComponent { Value = frontForwardFriction.ExtremumValue });
+            entity.SetComponent(new FrontAsymptoteSlipForwardComponent { Value = frontForwardFriction.AsymptoteSlip });
+            entity.SetComponent(new FrontAsymptoteValueForwardComponent { Value = frontForwardFriction.AsymptoteValue });
+            entity.SetComponent(new FrontStiffnessForwardComponent { Value = frontForwardFriction.Stiffness });
+
+            entity.SetComponent(new FrontExtremumSlipSidewaysComponent { Value = frontSidewaysFriction.ExtremumSlip });
+            entity.SetComponent(new FrontExtremumValueSidewaysComponent { Value = frontSidewaysFriction.ExtremumValue });
+            entity.SetComponent(new FrontAsymptoteSlipSidewaysComponent { Value = frontSidewaysFriction.AsymptoteSlip });
+            entity.SetComponent(new FrontAsymptoteValueSidewaysComponent { Value = frontSidewaysFriction.AsymptoteValue });
+            entity.SetComponent(new FrontStiffnessSidewaysComponent { Value = frontSidewaysFriction.Stiffness });
+        }
+
+        private void AddBackWheelComponents(Entity entity)
+        {
             var backMainWheelParameters = _carPreset.BackWheelParameters.MainWheelParameters;
             var backSuspensionSpring = _carPreset.BackWheelParameters.SuspensionSpring;
             var backForwardFriction = _carPreset.BackWheelParameters.ForwardFriction;
             var backSidewaysFriction = _carPreset.BackWheelParameters.SidewaysFriction;
-            
-            // Back components
-            entity.SetComponent(new BackWheelMassComponent{ Value = backMainWheelParameters.Mass});
-            entity.SetComponent(new BackWheelRadiusComponent{ Value = backMainWheelParameters.Radius});
-            entity.SetComponent(new BackDampingRateComponent{ Value = backMainWheelParameters.DampingRate});
-            entity.SetComponent(new BackSuspensionDistanceComponent{ Value = backMainWheelParameters.SuspensionDistance});
-            entity.SetComponent(new BackForceAppPointDistanceComponent{ Value = backMainWheelParameters.ForceAppPointDistance});
-            entity.SetComponent(new BackWheelCenterComponent{ Value = backMainWheelParameters.Center});
-            
-            entity.SetComponent(new BackSpringComponent{ Value = backSuspensionSpring.Spring});
-            entity.SetComponent(new BackDamperComponent{ Value = backSuspensionSpring.Damper});
-            entity.SetComponent(new BackTargetPositionComponent{ Value = backSuspensionSpring.TargetPosition});
-            
-            entity.SetComponent(new BackExtremumSlipForwardComponent{ Value = backForwardFriction.ExtremumSlip});
-            entity.SetComponent(new BackExtremumValueForwardComponent{ Value = backForwardFriction.ExtremumValue});
-            entity.SetComponent(new BackAsymptoteSlipForwardComponent{ Value = backForwardFriction.AsymptoteSlip});
-            entity.SetComponent(new BackAsymptoteValueForwardComponent{ Value = backForwardFriction.AsymptoteValue});
-            entity.SetComponent(new BackStiffnessForwardComponent{ Value = backForwardFriction.Stiffness});
-            
-            entity.SetComponent(new BackExtremumSlipSidewaysComponent{ Value = backSidewaysFriction.ExtremumSlip});
-            entity.SetComponent(new BackExtremumValueSidewaysComponent{ Value = backSidewaysFriction.ExtremumValue});
-            entity.SetComponent(new BackAsymptoteSlipSidewaysComponent{ Value = backSidewaysFriction.AsymptoteSlip});
-            entity.SetComponent(new BackAsymptoteValueSidewaysComponent{ Value = backSidewaysFriction.AsymptoteValue});
-            entity.SetComponent(new BackStiffnessSidewaysComponent{ Value = backSidewaysFriction.Stiffness});
+
+            entity.SetComponent(new BackWheelMassComponent { Value = backMainWheelParameters.Mass });
+            entity.SetComponent(new BackWheelRadiusComponent { Value = backMainWheelParameters.Radius });
+            entity.SetComponent(new BackDampingRateComponent { Value = backMainWheelParameters.DampingRate });
+            entity.SetComponent(new BackSuspensionDistanceComponent { Value = backMainWheelParameters.SuspensionDistance });
+            entity.SetComponent(new BackForceAppPointDistanceComponent { Value = backMainWheelParameters.ForceAppPointDistance });
+            entity.SetComponent(new BackWheelCenterComponent { Value = backMainWheelParameters.Center });
+
+            entity.SetComponent(new BackSpringComponent { Value = backSuspensionSpring.Spring });
+            entity.SetComponent(new BackDamperComponent { Value = backSuspensionSpring.Damper });
+            entity.SetComponent(new BackTargetPositionComponent { Value = backSuspensionSpring.TargetPosition });
+
+            entity.SetComponent(new BackExtremumSlipForwardComponent { Value = backForwardFriction.ExtremumSlip });
+            entity.SetComponent(new BackExtremumValueForwardComponent { Value = backForwardFriction.ExtremumValue });
+            entity.SetComponent(new BackAsymptoteSlipForwardComponent { Value = backForwardFriction.AsymptoteSlip });
+            entity.SetComponent(new BackAsymptoteValueForwardComponent { Value = backForwardFriction.AsymptoteValue });
+            entity.SetComponent(new BackStiffnessForwardComponent { Value = backForwardFriction.Stiffness });
+
+            entity.SetComponent(new BackExtremumSlipSidewaysComponent { Value = backSidewaysFriction.ExtremumSlip });
+            entity.SetComponent(new BackExtremumValueSidewaysComponent { Value = backSidewaysFriction.ExtremumValue });
+            entity.SetComponent(new BackAsymptoteSlipSidewaysComponent { Value = backSidewaysFriction.AsymptoteSlip });
+            entity.SetComponent(new BackAsymptoteValueSidewaysComponent { Value = backSidewaysFriction.AsymptoteValue });
+            entity.SetComponent(new BackStiffnessSidewaysComponent { Value = backSidewaysFriction.Stiffness });
         }
 
         public void OnUpdate(float deltaTime) { }
